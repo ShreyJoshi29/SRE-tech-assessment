9f7f0e1f0e73106edb26c0de6871c9d2581f60c4
Shrey Joshi <shreyjoshi29@gmail.com>
2024-02-27 18:04:45 -0500

    Added Dockerfile

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = 3000;
 
 // Define a route that responds with a simple message
 app.get('/', (req, res) => {
-  res.send('Hello, world!');
+  res.send('Hello, this is my simple web application for the tech assessment');
 });
 
 // Start the server
